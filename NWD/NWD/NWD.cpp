de8a4803e03732042330909887260d895cbdcc52
Squeeze-Sin <74215621+Squeeze-Sin@users.noreply.github.com>
2022-03-23 09:59:13 +0100

    Add files via upload

---
+++ NWD/NWD/NWD.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "Podaj pierwsza liczbe: ";
+	cin >> a;
+	cout << "Podaj druga liczbe: ";
+	cin >> b;
+
+	while (a != b)
+	{
+		if (a < b)
+		{
+			b = b - a;
+		}
+		else
+		{
+			a = a - b;
+		}
+	}
+	cout << "Najwiekszy wspolny dzielnik: " << a << endl;
+}
+
