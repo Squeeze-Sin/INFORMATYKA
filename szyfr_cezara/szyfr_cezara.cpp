f515488058d53aebd481729143673f5e5a40f182
Squeeze-Sin <74215621+Squeeze-Sin@users.noreply.github.com>
2022-04-25 20:19:44 +0200

    Add files via upload

---
+++ szyfr_cezara/szyfr_cezara.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+using namespace std;
+void szyfrowanie(int klucz, char tab[]);
+
+int main()
+{
+	int klucz;
+	char tab[200];
+
+	cout << "Podaj wyraz składajacy sie z malych liter: ";
+	cin >> tab;
+	cout << "Popaj klucz z przedzialu [-26..26]: ";
+	cin >> klucz;
+
+	szyfrowanie(klucz, tab);
+	cout << "Po zaszyfrowaniu: " << tab << endl;
+	szyfrowanie(-klucz, tab);
+	cout << "Po rozszyfrowaniu: " << tab << endl;
+}
+
+void szyfrowanie(int klucz, char tab[])
+{
+	int dl = strlen(tab);
+
+	if (!(klucz >= -26 && klucz <= 26))
+	{
+		return;
+	}
+
+	if (klucz >= 0)
+	{
+		for (int i = 0; i < dl; i++)
+		{
+			if (tab[i] + klucz <= 'z')
+			{
+				tab[i] += klucz;
+			}
+			else
+			{
+				tab[i] = tab[i] + klucz - 26;
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < dl; i++)
+		{
+			if (tab[i] + klucz >= 'a')
+			{
+				tab[i] += klucz;
+			}
+			else
+			{
+				tab[i] = tab[i] + klucz + 26;
+			}
+		}
+	}
+}
